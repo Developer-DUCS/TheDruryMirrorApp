f41c37a9124b86da67336afb43003ecc2a2b73c5
Sam <60146956+srudqvist@users.noreply.github.com>
2022-09-12 17:45:12 -0500

    Code runs on both android and iphone for Sam

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -52,7 +52,8 @@ const styles = StyleSheet.create({
   {
     backgroundColor: 'black',
     padding: 10,
-    display: 'block',
+    // display: 'block',
+    //display: 1,
   },
   headerText: 
   {
