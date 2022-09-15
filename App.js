568d7e82eae2a87ec4aba03362c05ed020c9397f
ssbbpeople <mcserverfeed321@gmail.com>
2022-09-15 18:25:02 -0500

    Text Editor misc.

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from "react-native";
 import { StatusBar } from 'expo-status-bar';
+import { Platform } from 'react-native';
+
+import { ArticleWritingPage } from './Pages/ArticleWriting';
+
 import { Entypo } from '@expo/vector-icons';
-import { HeaderBar } from './Components/Header';
-import { TextEditor } from './Components/TextEditor';
 import { globalStyles } from './Styles/Global';
 import { useFonts } from 'expo-font';
-import { TextInput } from "react-native";
-import { Platform } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,15 +16,7 @@ export default function App() {
 
   return (
     <View>
-      <HeaderBar/>
-      <View>
-        <Text style = { {fontFamily: 'Garamond-Regular'} }>
-          New York Times: King Charles III Expresses ‘Profound Sorrow’ Over Queen’s Death in First Speech
-        </Text>
-      </View>
-      <View>
-        <TextEditor></TextEditor>
-      </View>
+      <ArticleWritingPage></ArticleWritingPage>
     </View>
     
   );
--- Components/TextEditor.js
+++ Components/TextEditor.js
@@ -1,6 +1,7 @@
 // Components
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from "react";
+import {Dropdown} from 'react-native-element-dropdown';
 
 // Styles
 import * as Icon from "react-native-feather";
@@ -12,11 +13,75 @@ export function TextEditor()
     const [text, onChangeText] = React.useState("Write Here");
     const [number, onChangeNumber] = React.useState(null);
 
+    const [dropdown, setDropdown] = React.useState(null);
+        const [selected, setSelected] = React.useState([]);
+
+    const fontTypeData = [
+        {label: 'Times New Roman', value: '1'},
+        {label: 'Arial', value: '2'},
+        {label: 'Garamond Regular', value: '3'},
+    ];
+
+    const fontSizeData = [
+        {label: '6', value: '1'},
+        {label: '8', value: '2'},
+        {label: '12', value: '3'},
+        {label: '16', value: '4'},
+        {label: '18', value: '5'},
+        {label: '24', value: '6'},
+    ];
+
+    const _renderItem = item => {
+        return (
+        <View style={styles.item}>
+            <Text style={styles.textItem}>{item.label}</Text>
+        </View>
+        );
+    };
+
+
     return(
         <View>
                 <View style={[styles.optionsBar, globalStyles.primaryContainer]}>
                     <View>
-                        <Text style = {[globalStyles.paragraph, styles.optionsBarText]}>Fonts</Text>
+                        <Dropdown
+                            style={[styles.dropdown, styles.optionsBarText]}
+                            containerStyle={styles.shadow}
+                            data={fontTypeData}
+                            search
+                            searchPlaceholder="Search"
+                            labelField="label"
+                            valueField="value"
+                            label="Fonts"
+                            placeholder="Font"
+                            value={dropdown}
+                            onChange={item => {
+                            setDropdown(item.value);
+                                console.log('selected', item);
+                            }}
+                            renderItem={item => _renderItem(item)}
+                            textError="Error"
+                        />
+                    </View>
+                    <View>
+                        <Dropdown
+                            style={[styles.dropdown, styles.optionsBarText]}
+                            containerStyle={styles.shadow}
+                            data={fontSizeData}
+                            search
+                            searchPlaceholder="Search"
+                            labelField="label"
+                            valueField="value"
+                            label="Size"
+                            placeholder="Size"
+                            value={dropdown}
+                            onChange={item => {
+                            setDropdown(item.value);
+                                console.log('selected', item);
+                            }}
+                            renderItem={item => _renderItem(item)}
+                            textError="Error"
+                        />
                     </View>
                     <View>
                         <Text style = {[globalStyles.paragraph, styles.optionsBarText]}>B</Text>
@@ -34,13 +99,14 @@ export function TextEditor()
                         <Icon.AlignCenter style = {styles.optionsBarIcon}></Icon.AlignCenter>
                     </View>
                     <View>
-                        <Icon.AlignRight style = {styles.optionsBarIcon}></Icon.AlignRight>
+                        <Icon.AlignRight style = {styles.optionsBarIcon} multiline></Icon.AlignRight>
                     </View>
                 </View>
                 <TextInput
                     style={styles.input}
                     onChangeText={onChangeText}
                     value={text}
+                    multiline
                 />
         </View>
     )
@@ -48,7 +114,7 @@ export function TextEditor()
 
 const styles = StyleSheet.create({
     optionsBar: {
-        height: '40px',
+        height: '60px',
         margin: 12,
         marginBottom: 6,
         padding: 12,
@@ -57,25 +123,56 @@ const styles = StyleSheet.create({
         direction: 'column',
         flexDirection: 'row',
     },
+    dropdown: {
+        backgroundColor: 'white',
+        borderBottomColor: 'gray',
+        borderBottomWidth: 0.5,
+
+    },
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {
+        width: 0,
+        height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+    },
+    item: {
+        paddingVertical: 17,
+        paddingHorizontal: 4,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    textItem: {
+        flex: 1,
+        fontSize: 16,
+    },
     optionsBarText: {
         color: 'white',
         borderWidth: 1,
         padding: 3,
+        paddingLeft: 6,
+        paddingRight: 6,
         borderColor: 'white',
         fontStyle: 'bold',
     },
     optionsBarIcon: {
         color: 'white',
-        padding: 3,
         marginBottom: 3,
+        marginLeft: 6,
         borderColor: 'white',
         fontStyle: 'bold',
     },
     input: {
-      height: '80%',
+      height: '80vh',
+      textAlignVertical: 'top',
+      padding: 3,
+      paddingLeft: 5,
       margin: 12,
       marginTop: 0,
       borderWidth: 1,
-      padding: 10,
     },
   });
