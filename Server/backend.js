133e9bd84f4d896f692a1e6f666559208fb6711e
Sam <60146956+srudqvist@users.noreply.github.com>
2022-09-28 09:54:00 -0500

    started server

--- Server/backend.js
+++ Server/backend.js
@@ -9,3 +9,22 @@
 //   - Express initializes the backend application
 //   - Bodyparser is a middleware for the frontend and backend
 
+const express = require("express");
+const PORT = 3000;
+const app = express()
+
+//const router = express.Router()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Server Start Failed");
+    }
+    else {
+        console.log(`Server Listening on Port: ${PORT}`);
+    }
+});
