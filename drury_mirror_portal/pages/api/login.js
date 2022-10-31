017b4688faae82129f35baba6707ee6cb98895e5
tnield <60146942+tnield@users.noreply.github.com>
2022-10-31 09:35:34 -0500

    Added active or not filter on login.js

--- drury_mirror_portal/pages/api/login.js
+++ drury_mirror_portal/pages/api/login.js
@@ -1,4 +1,12 @@
-
+// login.js
+// Page Description:
+//                  The route that selects the user trying to login by match the username and password
+//Creation Date:
+//                  By: Thomas Nield, Daniel Brinck, Samuel Rudqvist  Oct. 4 2022 
+//
+//Modificaiton Log:
+//                  Added check for whether or not a user is active or deactive Oct. 31 2022
+//                   
 
 const conn = require("../../backend/mysqldb");
 
@@ -9,7 +17,7 @@ export default (req, res) => {
 
     let email = body.username
     let password = body.password
-    let authQuery = ("SELECT email, password, roles FROM users WHERE email = ?")
+    let authQuery = ("SELECT email, password, roles, active FROM users WHERE email = ?")
 
     conn.query(authQuery, [email], (err, rows) => {
 
@@ -20,7 +28,7 @@ export default (req, res) => {
             console.log("User Not Found")
         }
         else {
-            if (password == rows[0].password) {
+            if (password == rows[0].password && rows[0].active == 1) {
                 let user = {
                     username: rows[0].email,
                     role: rows[0].roles
@@ -34,11 +42,6 @@ export default (req, res) => {
             }
         }
 
-
-
-
     });
-
-
-
+    
 }
