942f997f3c33cf153835f1d8f90f75adb0b0b12b
itsthomthomob <mcserverfeed321@gmail.com>
2022-12-11 13:44:18 -0600

    Update about.js

--- drury_mirror_portal/pages/about.js
+++ drury_mirror_portal/pages/about.js
@@ -19,13 +19,13 @@ import Header from "./header";
 
 export default function about() {
     return (
-        <Box sx={{ height: "100%",backgroundColor: "#F3F3F3" }}>
+        <Box sx={{ height: "100%", backgroundColor: "#F3F3F3" }}>
             <Header />
             <Box sx={{ height: "100%", backgroundColor: "#F3F3F3" }}>
                 <Box sx={{ marginLeft: "10%", paddingTop: 3, width: "50%" }}>
                     <Typography sx={{ color: "black" }} variant="aboutHeader">
-                        Learn about how we are bringing innovation to the Drury
-                        Community and to Drury Mirror.
+                        Meet the team behind the Drury Mirror app and website
+                        portal.
                     </Typography>
                 </Box>
                 <Box sx={{ marginLeft: "10%", marginTop: 4, width: "50%" }}>
@@ -33,11 +33,11 @@ export default function about() {
                         We are Reflecting DUCS
                     </Typography>
                     <Typography variant="aboutBody">
-                        , the development team behind the Drury Mirror website
-                        and mobile app. We utilize cutting-edge full-stack
-                        development technology to bring together an entire
-                        website for Drury Mirror and a brand new mobile app for
-                        Drury students.
+                        . Using the skills we learned at Drury University from
+                        the Computer Science department, we have developed a
+                        website portal for the Drury Mirror to create, edit, and
+                        publish articles for their brand new app. This allows
+                        Drury students to be informed and consume news
                     </Typography>
                 </Box>
                 <Box sx={{ paddingLeft: "10%", marginTop: 4 }}>
@@ -53,7 +53,16 @@ export default function about() {
                         height: "30vh",
                     }}
                 >
-                    <Grid container columnSpacing={2} rowSpacing={2} sx={{ paddingBottom: 3, display: "flex", backgroundColor: "#F3F3F3" }}>
+                    <Grid
+                        container
+                        columnSpacing={2}
+                        rowSpacing={2}
+                        sx={{
+                            paddingBottom: 3,
+                            display: "flex",
+                            backgroundColor: "#F3F3F3",
+                        }}
+                    >
                         <Grid item>
                             <Card>
                                 <CardMedia
