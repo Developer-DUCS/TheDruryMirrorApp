411088debc12173ed9e383ddfbf0ed8a8b2fe4f8
itsthomthomob <mcserverfeed321@gmail.com>
2023-02-12 19:14:58 -0600

    sql

--- drury_mirror_portal/pages/header.js
+++ drury_mirror_portal/pages/header.js
@@ -21,8 +21,8 @@ export default function Header() {
     };
 
     const handleLogo = () => {
-        router.push("/testSplashPage")
-    }
+        router.push("/testSplashPage");
+    };
 
     // Handle the write draft button
     const writeDraftRoute = async (event) => {
@@ -36,25 +36,65 @@ export default function Header() {
 
     // Handle the About Us button
     const handleAbout = () => {
-        router.push("/about")
-    }
+        router.push("/about");
+    };
 
     return (
-        <Grid container sx={{ height: "8vh", marginBottom: 2, backgroundColor: "white", color: "black", display: "flex" }}>
-            <Grid item xs={3} sx={{marginTop: 1}}>
-                <Button variant="text" sx={{fontSize: "20px", fontFamily: "Trajan"}} onClick={() => {handleLogo();}}>
+        <Grid
+            container
+            sx={{
+                height: "8vh",
+                marginBottom: 2,
+                backgroundColor: "white",
+                color: "black",
+                display: "flex",
+            }}>
+            <Grid
+                item
+                xs={4}
+                sx={{ marginTop: 1 }}>
+                <Button
+                    variant="text"
+                    sx={{
+                        fontSize: { lg: "18px", md: "16px", sm: "14px" },
+                        fontFamily: "Trajan",
+                    }}
+                    onClick={() => {
+                        handleLogo();
+                    }}>
                     Drury Mirror
                 </Button>
             </Grid>
-            <Grid item xs={6}></Grid>
-            <Grid item xs={3} sx={{marginTop: 1}}>
-                <Button sx={{color: "white", marginRight: 2}} variant="contained" color="primaryButton" onClick={writeDraftRoute}>
+            <Grid
+                item
+                xs={8}
+                sx={{
+                    marginTop: 1,
+                    fontSize: { lg: "16px", md: "14px", sm: "12px" },
+                }}>
+                <Button
+                    sx={{ color: "white", marginRight: 2, fontSize: { lg: "16px", md: "14px", sm: "12px" } }}
+                    variant="contained"
+                    color="primaryButton"
+                    onClick={writeDraftRoute}>
                     Write Draft
                 </Button>
-                <Button sx={{color: "white", marginRight: 2}} variant="contained" color="primaryButton" onClick={handleAbout}>
+                <Button
+                    sx={{
+                        color: "white",
+                        marginRight: 2,
+                        fontSize: { lg: "16px", md: "14px", sm: "12px" },
+                    }}
+                    variant="contained"
+                    color="primaryButton"
+                    onClick={handleAbout}>
                     About Us
                 </Button>
-                <Button variant="outlined" color="error" onClick={logOut}>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    sx={{ fontSize: { lg: "16px", md: "14px", sm: "12px" } }}
+                    onClick={logOut}>
                     Log Out
                 </Button>
             </Grid>
