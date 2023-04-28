06d6c69653965f440f72d169e589516ad94082de
Sam <60146956+srudqvist@users.noreply.github.com>
2023-04-28 15:18:08 -0500

    Added mobile search api

--- drury_mirror_portal/pages/api/mobileAppSearch.js
+++ drury_mirror_portal/pages/api/mobileAppSearch.js
@@ -24,42 +24,22 @@ function runMiddleware(req, res, fn) {
 export default async (req, res) => {
 	await runMiddleware(req, res, cors);
 	if (req.method === "POST") {
+		let searchText = req.body.searchText;
 		const isDraft = "5";
 
-		let getPublished =
-			"SELECT aid,author,headline,body,isDraft FROM articles WHERE isDraft = ?";
-		let getTags = "SELECT * FROM tags WHERE tid = ?";
-
+		let searchQuery = `SELECT * FROM articles WHERE headline LIKE '%${searchText}%' AND isDraft = ?;`;
 		let articlesResult = await executeQuery({
-			query: getPublished,
+			query: searchQuery,
 			values: isDraft,
 		});
-
 		if (articlesResult.error) {
 			return res
 				.status(500)
 				.json({ error: "There was an error loading the articles" });
 		} else {
-			let articles = [];
-			let tagsList = [];
-			for (let i = 0; i < articlesResult.length; i++) {
-				let tags = await executeQuery({
-					query: getTags,
-					values: articlesResult[i].aid,
-				});
-				if (tags.error) {
-					console.log(tags.error);
-				} else {
-					tagsList.push(tags);
-				}
-				articles.push(articlesResult[i]);
-			}
-
-			return res
-				.status(200)
-				.json({ result: articlesResult, tagsList: tagsList });
+			return res.status(200).json({ result: articlesResult });
 		}
 	} else {
-		res.status(400).json({ msg: "Invalid Request" });
+		return res.status(400).json({ msg: "Invalid Request" });
 	}
 };
