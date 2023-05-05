2d6a90696d7d283b724fd797d2c2b0670a14871c
Sam <60146956+srudqvist@users.noreply.github.com>
2023-05-05 12:04:27 -0500

    moved redux

---
+++ redux/actions.js
@@ -0,0 +1,13 @@
+// redux/actions.js
+export const SET_CURRENT_ARTICLE = "SET_CURRENT_ARTICLE";
+export const SET_ARTICLE_DATA = "SET_ARTICLE_DATA";
+
+export const setCurrentArticle = (slug) => ({
+	type: SET_CURRENT_ARTICLE,
+	payload: slug,
+});
+
+export const setArticleData = (articleData) => ({
+	type: SET_ARTICLE_DATA,
+	payload: articleData,
+});
